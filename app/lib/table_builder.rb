c04f752c93208e0524a35833cfcf9dd526573c12
Kevin <kevin.k1252@gmail.com>
2025-08-12 13:44:23 +0200

    cleanups

--- app/lib/table_builder.rb
+++ app/lib/table_builder.rb
@@ -2,7 +2,7 @@ class TableBuilder
   include FiguresHelper
 
   def build(grains)
-    attributes = ["Befund",	"Probennummer", "Species", "SIA #", "Length 1 (mm)", "Length 2 (mm)", "Average length", "Breadth 1 (mm)", "Breadth 2 (mm)", "Average breadth", "Thickness 1 (mm)", "Thickness 2 (mm)", "Average thickness (mm)", "Altura foto 1", "Altura foto 2", "Average thickness (photo)", "KW 1: L x B", "KW 2: L x T", "KW 1", "KW 2", "Weight (mg)", "(KW1 + KW 2)/2"]
+    attributes = ["Befund",	"Sample", "Species", "Length 1 (mm)", "Length 2 (mm)", "Average length", "Breadth 1 (mm)", "Breadth 2 (mm)", "Average breadth", "Thickness (mm)"]
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
@@ -36,26 +36,13 @@ class TableBuilder
           grain.upload.feature,
           grain.identifier,
           grain.strain.name,
-          "",
           number_with_unit(dorsal_length), # length 1
           number_with_unit(ventral_length), # length 2
           number_with_unit({unit: "mm", value: average_length}),
           number_with_unit(dorsal_width), # breadth 1
           number_with_unit(ventral_width), # breadth 2
           number_with_unit({unit: "mm", value: average_breadth}), # average breadth
-          number_with_unit(thickness), # thickness 1
-          "", # thickness 2
-          "", # average thickness
-          "", # altura 1
-          "", # altura 2
-          "", # average altura
-          "", # average thickness photo
-          safe_mul(average_length, average_breadth), # L x B
-          safe_mul(average_length, thickness), # L x T
-          "", # KW 1
-          "", # KW 2
-          "", # Weight,
-          "" # (KW1 + KW2) / 2
+          number_with_unit(thickness), # thickness
         ]
       end
     end
