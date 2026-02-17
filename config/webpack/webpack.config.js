2c9fdcf8f8e96f751a94beae9f60a38aecafcb1d
Kevin <kevin.k1252@gmail.com>
2026-02-17 21:12:24 +0100

    added scw

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,14 +1,14 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker');
+const { generateWebpackConfig } = require('shakapacker')
 
 const customConfig = {
   module: {
     rules: [
-      { test: /\.res$/, loader: 'ignore-loader' },
+      { test: /\.res$/, loader: 'ignore-loader' }
     ]
   }
-};
+}
 
-const webpackConfig = generateWebpackConfig(customConfig);
+const webpackConfig = generateWebpackConfig(customConfig)
 
-module.exports = webpackConfig;
+module.exports = webpackConfig
